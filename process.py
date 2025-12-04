da4f6211fab3fcf128fcb4401d913a8e3c0accd5
Nikoleta Papadopoulou <nikolpanpap@gmail.com>
2025-12-04 21:51:42 +0100

    Detecting some of the grid lines not all yet

--- process.py
+++ process.py
@@ -5,6 +5,8 @@ Kainourgio repository: https://github.com/nikolpapad/grid_extraction.git
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.widgets import Slider
+
 
 img_path =r"c:\Users\nikol\Downloads\page_93.png"
 img = cv2.imread(img_path)
@@ -25,15 +27,27 @@ otsu = cv2.threshold(gray_blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 # plt.show()
 
 binary_copy = otsu.copy()
+orig = img.copy() # original grayscale or color image
 height, width = binary_copy.shape
+maxL = int(max(height, width) )
 
+edges = cv2.Canny(binary_copy, 50, 150, apertureSize=3)
+# Standard Hough Line 
+lines = cv2.HoughLinesP(edges, 1, np.pi / 180, threshold=250, minLineLength=20, maxLineGap=30)
+print(f"Detected {0 if lines is None else len(lines)} lines")
 
-# Kernel width relative to image width (tune this!)
-vertical_kernel_len = max(10, height // 40)  # adjust if needed
-
-# Vertical kernel: tall and thin
-vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, vertical_kernel_len))
-
-# Erode then dilate to keep only strong vertical segments
-vertical_lines = cv2.erode(binary_copy, vertical_kernel, iterations=1)
-vertical_lines = cv2.dilate(vertical_lines, vertical_kernel, iterations=1)
+if lines is not None:
+    atol = 1.0  # angle tolerance in degrees
+    for x1, y1, x2, y2 in lines[:, 0, :]:
+        dx, dy = (x2 - x1), (y2 - y1)
+        angle = (np.degrees(np.arctan2(dy, dx)) + 180.0) % 180.0
+        near_horizontal = min(angle, 180.0 - angle) < atol
+        near_vertical   = abs(angle - 90.0) < atol
+        # draw only near-horizontal/vertical lines
+        if near_horizontal or near_vertical:
+            cv2.line(orig, (x1, y1), (x2, y2), (255, 0, 0), 5)
+        
+    plt.figure(figsize=(10, 10))
+    plt.imshow(cv2.cvtColor(orig, cv2.COLOR_BGR2RGB))
+    plt.title("Detected Grid Lines Overlay")
+    plt.show()
