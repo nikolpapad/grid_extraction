601a74b0a3d6e320bf1de59bf7212058135413e3
Nikoleta Papadopoulou <nikolpanpap@gmail.com>
2025-12-04 16:53:41 +0100

    working with manually cropped image

---
+++ process.py
@@ -0,0 +1,7 @@
+''' Teleutaia prospatheia gia grid-extraction pipeline.
+Kainourgio repository: https://github.com/nikolpapad/grid_extraction.git
+
+'''
+import cv2
+import numpy as np
+img_path =r"c:\Users\nikol\Downloads\page_93.png"
